1e6f3bb81b433d6cb4f1fa00a12bfc2516461329
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2016-02-23 00:14:39 -0300

    Outlines the goal object

--- app/client/js/main.js
+++ app/client/js/main.js
@@ -1,1 +1,18 @@
 
+var app = angular.module('dumb-goals', []);
+app.controller('mainCtrl', function($scope) {
+    $scope.categories = {
+
+    };
+
+    $scope.goals = [{
+		category:
+		name:
+		mensuravel:
+		tasks:
+		reason:
+		timing:
+		status:
+    }];
+
+});
